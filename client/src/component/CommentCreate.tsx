3543189c380ab49a4b142f81f6535be09f0e68b9
Mictim <mictim2002@gmail.com>
2023-01-24 13:30:23 +0100

    Added react app

---
+++ client/src/component/CommentCreate.tsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+
+type CommentCreateProps = {
+    postId: string;
+}
+
+const CommentCreate = ({ postId }: CommentCreateProps) => {
+    const [content, setContent] = useState('');
+
+    const onSubmit = async(event: React.FormEvent) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('');
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>NewComment</label>
+                    <input value={content} onChange={e => setContent(e.target.value)} className="form-control" />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentCreate;
